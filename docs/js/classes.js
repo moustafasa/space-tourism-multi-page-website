43eccaec954da51cfa217716ecc82ef6186ed4b7
moustafa <moustafasaad954@gmail.com>
2022-09-17 03:21:03 +0200

    make slider code

--- docs/js/classes.js
+++ docs/js/classes.js
@@ -1,19 +1,90 @@
 class apiConnect {
-  constructor(page) {
-    this.page = page;
+  constructor(choosed) {
+    this.choosed = choosed;
+  }
+  async #connect() {
+    this.response = await fetch("./data.json");
+    this.data = await this.response.json();
   }
   async destination() {
-    let data = await fetch("./data.json");
-
-    console.log(await data.json());
+    await this.#connect();
+    this.final = this.data["destinations"].find(
+      (ele) => ele.name.toLowerCase() === this.choosed.toLowerCase()
+    );
+    return this.final;
+  }
+  async crew() {
+    await this.#connect();
+    this.final = this.data["crew"];
+    return this.final;
   }
 }
 class template {
-  constructor(data) {
-    this.data = data;
+  static id = 0;
+  constructor() {}
+  destination(data) {
+    // get element from document
+    this.imageEle = document.querySelector(".destination .left .img img");
+    this.nameEle = document.querySelector(".destination .content .name");
+    this.descEle = document.querySelector(".destination .content .description");
+    this.distance = document.querySelector(
+      ".destination .content .measures .distance .num"
+    );
+    this.travel = document.querySelector(
+      ".destination .content .measures .travel-time .num"
+    );
+    // fill element with data
+    this.imageEle.src = data.images.png;
+    this.nameEle.innerText = data.name;
+    this.descEle.innerText = data.description;
+    this.distance.innerText = data.distance;
+    this.travel.innerText = data.travel;
   }
-  destination() {}
+  crew(data) {
+    // get parent of boxes
+    this.content = document.querySelector(".crew .info-cont");
+    this.imgCont = document.querySelector(".crew .img");
+    // create elements
+    this.box = document.createElement("div");
+    this.box.classList.add("box");
+    this.box.id = template.id;
+    this.role = document.createElement("span");
+    this.role.classList.add("role", ".f-600");
+    this.name = document.createElement("h2");
+    this.name.classList.add("name", "f-700");
+    this.bio = document.createElement("p");
+    this.bio.classList.add("bio", "f-400");
+    this.img = document.createElement("img");
+    this.img.id = template.id++;
+    // append elements
+    this.box.append(this.role, this.name, this.bio);
+    this.content.append(this.box);
+    this.imgCont.append(this.img);
+
+    // fill elements
+    this.role.innerText = data.role;
+    this.name.innerText = data.name;
+    this.bio.innerText = data.bio;
+    this.box.dataset.name = data.name;
+    this.img.dataset.name = data.name;
+    this.img.src = data.images.png;
+  }
 }
 
-let api = new apiConnect("home");
-api.destination();
+export class add {
+  constructor(choosed) {
+    this.choosed = choosed;
+    this.api = new apiConnect(choosed);
+    this.template = new template();
+  }
+  async destination() {
+    this.data = await this.api.destination();
+    this.template.destination(this.data);
+  }
+  async crew() {
+    this.data = await this.api.crew();
+    this.data.forEach((cr) => {
+      this.template.crew(cr);
+    });
+  }
+}
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,19 @@
+import { add } from "./classes.js";
 let page = document.body.dataset.page;
 let navLinks = [...document.querySelectorAll("nav a")];
 let bars = document.querySelector(".bars");
 
+if (localStorage["page"]) {
+  page = localStorage["page"];
+  navLinks.forEach((ele) => {
+    if (ele.dataset.page === page) {
+      ele.classList.add("active");
+    } else {
+      ele.classList.remove("active");
+    }
+  });
+  document.body.dataset.page = page;
+}
 // navbar events
 navLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
@@ -20,6 +32,9 @@ navLinks.forEach((link) => {
     if (bars.classList.contains("active")) {
       bars.classList.remove("active");
     }
+
+    // add to localstorage
+    localStorage["page"] = page;
   });
 });
 
@@ -34,3 +49,76 @@ bars.querySelector(".close").addEventListener("click", (e) => {
   e.stopPropagation();
   bars.classList.remove("active");
 });
+
+// change page content on tab change
+let tabs = [...document.querySelectorAll(".tabs li")];
+tabs.forEach((tab) => {
+  if (tab.classList.contains("active")) {
+    let addApp = new add(tab.dataset.dest);
+    addApp.destination();
+  }
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    e.currentTarget.classList.add("active");
+    let addApp = new add(e.currentTarget.dataset.dest);
+    addApp.destination();
+  });
+});
+
+//fill page content on slider-1
+let sliders = [...document.querySelectorAll(".crew .slider-1 li")];
+let crewApp = new add();
+let crew = crewApp.crew();
+let boxes;
+crew.then(() => {
+  boxes = [...document.querySelectorAll(".crew :is(.box,.img img)")];
+  sliders.forEach((slide) => {
+    // show active slide
+    if (slide.classList.contains("active")) {
+      // show active member data
+      let data = document.querySelectorAll(
+        `.crew :is(img,.box)[data-name='${slide.dataset.name}']`
+      );
+      data.forEach((ele) => ele.classList.add("showed"));
+    }
+    // change data on click on slider
+    slide.addEventListener("click", (ev) => {
+      showData(ev.currentTarget);
+    });
+  });
+
+  // change data with slide on touchphones
+  let start;
+  document.body.addEventListener("touchstart", (e) => {
+    // get first touch event
+    start = e.changedTouches[0].screenX;
+  });
+  document.body.addEventListener("touchend", (e) => {
+    let active = sliders.indexOf(
+      sliders.find((ele) => ele.classList.contains("active"))
+    );
+    if (e.changedTouches[0].screenX > start) {
+      if (active < sliders.length - 1) {
+        showData(sliders[active + 1]);
+      }
+    } else if (e.changedTouches[0].screenX < start) {
+      if (active > 0) {
+        showData(sliders[active - 1]);
+      }
+    }
+  });
+});
+
+function showData(slide) {
+  sliders.forEach((ele) => ele.classList.remove("active"));
+  slide.classList.add("active");
+  boxes.forEach((box) => {
+    box.classList.remove("showed");
+    if (box.id == slide.id) {
+      box.classList.remove("slided");
+      box.classList.add("showed");
+    } else if (box.id < slide.id) {
+      box.classList.add("slided");
+    }
+  });
+}
